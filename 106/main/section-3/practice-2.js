20de6a69e1a5c335f2f3358459a338f9ca83d783
xiaoyzozi123 <1760228390@qq.com>
2017-11-06 21:27:37 +0500

    tijiao

--- 106/main/section-3/practice-2.js
+++ 106/main/section-3/practice-2.js
@@ -1,16 +1,15 @@
 'use strict';
 
-module.exports = function createUpdatedCollection(collectionA, objectB) {
-
-    for(let i = 0; i < collectionA.length;i++){
-        for(let j = 0;j < objectB.value.length; j++){
-           if(collectionA[i].key === objectB.value[j]){
-               if(collectionA[i].count / 3 >= 0){
-                   collectionA[i].count -= Math.floor(collectionA[i].count / 3);
-               }
-
-           }
+module.exports = function countSameElements(collectionA, objectB) {
+    for(let i = 0;i < collectionA.length;i++){
+        for(let j = 0; j < objectB.value.length; j++){
+            if(collectionA[i].key === objectB.value[j]){
+                if(collectionA[i].count / 3 >= 1){
+                    collectionA[i].count -= Math.floor(collectionA[i].count / 3);
+                }
+            }
         }
+
     }
-  return collectionA;
+    return collectionA;
 }
